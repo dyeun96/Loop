4331215182397d2988ce903b7180d18eba41136d
dyeun96 <dyeun96@naver.com>
2014-09-11 15:37:43 +0900

    a

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ function setup() {
   noFill();
 
   stroke(255, 0, 0);
-
-  var x;
+/ *
+    var x;
 
   x = 100;
   ellipse(x, 100, 50, 50);
@@ -15,4 +15,11 @@ function setup() {
 
   x = 300;
   ellipse(x, 100, 50, 50);
+}
+
+* /
+ for (var x = 1; x <= 8; x++)
+ {
+  ellipse(x * 70, x * 20 + 100, 50, 50);
+ }
 }
